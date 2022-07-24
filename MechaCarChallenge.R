b0f900100cab423aa54be90ed2eab91c99a40e0a
Krysdw <102800315+Krysdw@users.noreply.github.com>
2022-07-24 17:18:28 -0400

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,23 @@
+library(dplyr)
+MechaCar_mpg <-read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors =F)
+head(MechaCar_mpg)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle +ground_clearance + AWD ,data=MechaCar_mpg)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD ,data=MechaCar_mpg))
+
+#Deliv.2
+> Suspension_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) #import the csv and read as a table
+
+total_summary <-Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
+
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median = median(PSI), Variance = var(PSI), SD = sd(PSI) , .groups = 'keep')
+
+#Deliv.3
+t.test(Suspension_Coil$PSI,mu=mean(Suspension_Coil$PSI))
+
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot1"),mu=mean(Suspension_Coil$PSI)) #Lot1 t-test
+
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot2"),mu=mean(Suspension_Coil$PSI)) #Lot2 t-test
+
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot3"),mu=mean(Suspension_Coil$PSI)) #Lot3 t-test
+
